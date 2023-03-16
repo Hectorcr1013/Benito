f2fa7c66c51f0d65cebf8994c8ada8c3a6c2dc9a
chiqu <chiqu@192.168.0.132>
2023-03-16 11:48:39 -0700

    Se agrego el enumerador estado y se crearon los atributos a la orden de compra

--- src/mx/itson/benito/entidades/OrdenCompra.java
+++ src/mx/itson/benito/entidades/OrdenCompra.java
@@ -4,10 +4,135 @@
  */
 package mx.itson.benito.entidades;
 
+import java.util.Date;
+import java.util.List; 
+import mx.itson.benito.enumeradores.Estado;
+
 /**
  *
  * @author Hector
  */
 public class OrdenCompra {
+
+    private int id;
+    private Date fecha;
+    private double subtotal;
+    private double total;
+    private List<Articulo> articulos;
+    private Estado estado;
+    private List<Proveedor> proveedores;
+    private String folio;
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the subtotal
+     */
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    /**
+     * @param subtotal the subtotal to set
+     */
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    /**
+     * @return the total
+     */
+    public double getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the articulos
+     */
+    public List<Articulo> getArticulos() {
+        return articulos;
+    }
+
+    /**
+     * @param articulos the articulos to set
+     */
+    public void setArticulos(List<Articulo> articulos) {
+        this.articulos = articulos;
+    }
+
+    /**
+     * @return the estado
+     */
+    public Estado getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the proveedores
+     */
+    public List<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    /**
+     * @param proveedores the proveedores to set
+     */
+    public void setProveedores(List<Proveedor> proveedores) {
+        this.proveedores = proveedores;
+    }
+
+    /**
+     * @return the folio
+     */
+    public String getFolio() {
+        return folio;
+    }
+
+    /**
+     * @param folio the folio to set
+     */
+    public void setFolio(String folio) {
+        this.folio = folio;
+    }
     
 }
