202990bb2a8653fa7dcd85058cc7374591fc9dc4
Hector <Hector@192.168.0.31>
2023-03-21 22:13:12 -0700

    Creacion del paquete ui y sus clases

---
+++ src/mx/itson/benito/ui/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.benito.ui;
+
+import mx.itson.benito.persistencias.RelacionDAO;
+
+/**
+ *
+ * @author Hector
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        RelacionDAO.obtenerTodos(); 
+    }
+    
+}
